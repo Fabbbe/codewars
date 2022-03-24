d75f69b9f89cae7622fd9c13dbff586eb63e6b03
Fabbbe <fabian@fabbbe.se>
2022-03-24 13:18:35 +0100

    added printer_error.c

---
+++ c/printer_error.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char* printerError(char* s) {
+	int error_count = 0;
+	int s_len = strlen(s);
+	char* result_string = malloc(16*sizeof(char)); 
+
+	for (int i = 0; i < s_len; ++i) {
+		if (s[i] < 'a' || s[i] > 'm') {
+			++error_count;
+		}
+	}
+
+	sprintf(result_string, "%d/%d", error_count, s_len);
+
+	return result_string;
+}
